8bf53e23667911a5bd8ea2c00f704dd7ecfab0c0
vbungay <113390913+vbungay@users.noreply.github.com>
2023-03-15 19:44:20 -0400

    header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -30,7 +30,7 @@ const Header = () => {
     };
 
   return (
-    <header className='fixed z-50 w-screen p-6 px-16'>
+    <header className='fixed z-50 w-screen p-3 px-4 md:p-6 md:px-16'>
         <div className='hidden md:flex w-full h-full items-center justify-between'>
             {/* header logo */}
             <Link to={'/'} className='flex items-center gap-2'>
@@ -94,7 +94,54 @@ const Header = () => {
                 </motion.div>
             </div>
         </div>
-        <div className='flex md:hidden w-full h-full p-4'></div>
+        <div className='flex items-center justify-between md:hidden w-full h-full p-4'>
+            {/* mobile view header logo */}
+            <Link to={'/'} className='flex items-center gap-2'>
+                <img src={ Logo } className='w-20 object-cover' alt="logo" />
+                <p className='text-headingColor text-xl font-bold'>Shoe Ground</p>
+            </Link>
+            {/* mobile view user avatar */}
+            <motion.div 
+                    initial={{ opacity: 0, x: 150}}
+                    animate={{ opacity: 1, x: 0}}
+                    exit={{ opacity: 0, x: 150}} 
+                    className='relative'
+                >
+                    <motion.img whileTap={{scale: 0.7}} 
+                                src={ user ? user.photoURL: Avatar } 
+                                className='w-8 min-w-[38px] h-8 min-h-[38px] rounded-full' 
+                                alt="user"
+                                referrerPolicy="no-referrer"
+                                onClick={handleLogin} 
+                    />
+                    {/* if user is logged in, logout will appear upon clicking the avatar icon */}
+                    {
+                        isMenu && (
+                            <motion.div className='flex flex-col w-40 bg-gray-50 shadow-xl rounded-lg absolute top-10 right-0'>
+                                <ul
+                                    initial={{ opacity: 0, x: 150}}
+                                    animate={{ opacity: 1, x: 0}}
+                                    exit={{ opacity: 0, x: 150}}
+                                    className='flex flex-col gap-1'
+                                    >
+                                    <li className='text-base text-textColor hover:text-headingColor hover:bg-slate-100 px-4 py-2 duration-100 transition-all ease-in-out cursor-pointer'>
+                                        Home
+                                    </li>
+                                    <li className='text-base text-textColor hover:text-headingColor hover:bg-slate-100 px-4 py-2 duration-100 transition-all ease-in-out cursor-pointer'>
+                                        Products
+                                    </li>
+                                    <li className='text-base text-textColor hover:text-headingColor hover:bg-slate-100 px-4 py-2 duration-100 transition-all ease-in-out cursor-pointer'>
+                                        About Us
+                                    </li>
+                                </ul>
+                                <p className='px-4 py-3 flex items-center gap-3 cursoe-pointer hover:bg-slate-100 transition-all duration-100 ease-in-out text-textColor text-base'>
+                                    Logout <AiOutlineLogout />
+                                </p>
+                            </motion.div>
+                        )
+                    }
+                </motion.div>
+        </div>
     </header>
   )
 }
